25b739044dcd976b06e0d4d434828168296d44cb
kira aziz <kiraazziz2020@gmail.Com>
2024-03-10 19:00:35 +0100

    'first'

--- api/index.py
+++ api/index.py
@@ -1,20 +1,11 @@
-from scarper_logic import use_get_all_categories, use_get_category_total_pages, use_get_category_items, use_get_item_details
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/all-category')
-def list():
-    return use_get_all_categories()
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route('/category/page/<string:id>')
-def page(id):
-    return {"total" : use_get_category_total_pages(id)}
-
-@app.route('/category/items/<string:id>')
-def items(id):
-    return {"total" : use_get_category_items(id)}
-
-@app.route('/item/<string:id>')
-def item(id):
-    return use_get_item_details(id)
+@app.route('/about')
+def about():
+    return 'About'
